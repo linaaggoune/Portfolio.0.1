406f971520baec1cc4b002faab3508595bab81c9
lina <lina.aggn@gmail.com>
2025-02-27 08:42:03 +0100

    add i18n translation

--- frontend-librarie/src/App.tsx
+++ frontend-librarie/src/App.tsx
@@ -1,12 +1,22 @@
 import { Route, Routes } from "react-router-dom";
 
-import IndexPage from "@/pages/index";
-import AboutPage from "@/pages/about";
-import SkillsPage from "@/pages/skills.tsx";
-import ProjectsPage from "@/pages/projects.tsx";
-import ContactPage from "@/pages/contact.tsx";
+import IndexPage from "./pages/index";
 
+import AboutPage from "./pages/about";
+import SkillsPage from "./pages/skills.tsx";
+import ProjectsPage from "./pages/projects.tsx";
+import ContactPage from "./pages/contact.tsx";
+
+import { useEffect } from "react";
+import i18n from "./i18n";
 function App() {
+  console.log("App component loaded!");
+  useEffect(() => {
+    document.documentElement.dir = i18n.language === "ar" ? "rtl" : "ltr";
+    document.documentElement.lang = i18n.language;
+    console.log(document.documentElement.lang, "hellooo");
+  }, [i18n.language]);
+
   return (
     <Routes>
       <Route element={<IndexPage />} path="/" />
